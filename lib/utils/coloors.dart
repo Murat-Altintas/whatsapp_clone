af82444dfd706ad988c266c63ad9eef55855427c
Murat Altintas <murat.altintas.34@gmail.com>
2023-08-07 01:29:01 +0300

    first setup

---
+++ lib/utils/coloors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Coloors {
+  Coloors._();
+
+  static const Color lightGreen = Colors.teal;
+  static const Color lightBlue = Colors.blueAccent;
+  static const Color lightGrey = Colors.grey;
+  static const Color chatBG = Colors.blueGrey;
+  static const Color chatBubbleGreen = Colors.greenAccent;
+  static const Color chatBubbleWhite = Colors.white70;
+  static const Color errorColor = Colors.red;
+}
